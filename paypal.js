6d201fc98a34ec19e4775721b61e738b0c9d5f16
Liping Ruan <hello@ruanmail.com>
2022-03-24 17:13:24 +0800

    接入确认收款/退款/回调验证

--- paypal.js
+++ paypal.js
@@ -30,6 +30,9 @@ module.exports = class Paypal extends EventEmitter {
         clientId: '',
         clientSecret: '',
 
+        // paypal 应用详情内 sandbox webhooks 可查看
+        webhookId: '',
+
         // 店铺名称
         brandName: '',
 
@@ -76,9 +79,15 @@ module.exports = class Paypal extends EventEmitter {
     urls = {
         [secured]: this.urlPrefix,
         get authentication ( ) { return this[secured] + '/v1/oauth2/token' },
-        get createOrder ( ) { return this[secured] + '/v2/checkout/orders' },
-        get captureOrder ( ) { return this[secured] + '/v2/checkout/orders/${id}/capture' }
+        get createOrder    ( ) { return this[secured] + '/v2/checkout/orders' },
+        get getOrder       ( ) { return this[secured] + '/v2/checkout/orders/${id}' },
+        get captureOrder   ( ) { return this[secured] + '/v2/checkout/orders/${id}/capture' },
+        get getCapture     ( ) { return this[secured] + '/v2/payments/captures/${id}' },
+        get refundCapture  ( ) { return this[secured] + '/v2/payments/captures/${id}/refund' },
+        get getRefunds     ( ) { return this[secured] + '/v2/payments/refunds/${id}' },
+        get webhookVerify  ( ) { return this[secured] + '/v1/notifications/verify-webhook-signature' },
     }
+
 
 
     /**
@@ -112,6 +121,7 @@ module.exports = class Paypal extends EventEmitter {
 
         return access_token
     }
+
 
 
     /**
@@ -164,6 +174,47 @@ module.exports = class Paypal extends EventEmitter {
 
 
 
+    /**
+     * webhook notification signature verification
+     * @param headers
+     * @param event
+     * @returns {Promise<boolean>}
+     */
+    async webhookVerify ( headers, event ) {
+
+        const { webhookId } = this.#config
+
+        const body = {
+            transmission_id: headers [ 'paypal-transmission-id' ],
+            transmission_time: headers [ 'paypal-transmission-time' ],
+            cert_url: headers [ 'paypal-cert-url' ],
+            auth_algo: headers [ 'paypal-auth-algo' ],
+            transmission_sig: headers [ 'paypal-transmission-sig' ],
+            webhook_id: webhookId,
+            webhook_event: event
+        }
+
+        const { body: { verification_status } } = await IO.http ( {
+            url: this.urls.webhookVerify,
+            headers: await this.genHeaders ( )
+        }, body )
+
+        return verification_status === 'SUCCESS'
+    }
+
+
+
+    /**
+     * 创建支付订单
+     * @param params
+     * @param {string} params.orderId 订单号
+     * @param {number} params.amount 交易金额
+     * @param {string} params.description 交易描述
+     * @param {*} params.attach 回调附加参数
+     * @param {string} params.returnUrl 付款成功页面
+     * @param {string} params.cancelUrl 取消付款页面
+     * @returns {Promise<{id: string, paymentURL:string}>}
+     */
     async createOrder ( params ) {
 
         const { orderId, amount, description, attach, returnUrl, cancelUrl } = params
@@ -182,7 +233,7 @@ module.exports = class Paypal extends EventEmitter {
         if ( attach ) purchase.custom_id = 'string' === typeof attach ? attach : JSON.stringify ( attach )
 
         const body = {
-            intent: 'CAPTURB',
+            intent: 'CAPTURE',
             purchase_units: [ purchase ],
             application_context: {
                 brand_name: brandName,
@@ -212,6 +263,11 @@ module.exports = class Paypal extends EventEmitter {
 
 
 
+    /**
+     * 确认收款
+     * @param {string} id 订单号
+     * @returns {Promise<*>}
+     */
     async captureOrder ( id ) {
 
         const { body } = await IO.http ( { 
@@ -229,6 +285,84 @@ module.exports = class Paypal extends EventEmitter {
         }
 
         if ( status !== 'COMPLETED' ) throw new Error ( 'ORDER_NOT_COMPLETED' )
+
+        return body
+    }
+
+
+
+    /**
+     * Show order details
+     * @param {string} id order id
+     * @returns {Promise<*>}
+     */
+    async getOrder ( id ) {
+
+        const { body } = await IO.http ( {
+            url: this.urls.getOrder.replace ( '${id}', id ),
+            headers: await this.genHeaders ( )
+        } )
+
+        return body
+    }
+
+
+
+    /**
+     * Show captured payment details
+     * @param {string} id capture id
+     * @returns {Promise<*>}
+     */
+    async getCapture ( id ) {
+
+        const { body } = await IO.http ( {
+            url: this.urls.getCapture.replace ( '${id}', id ),
+            headers: await this.genHeaders ( )
+        } )
+
+        return body
+    }
+
+
+
+    /**
+     * Refund captured payment
+     * @param {string} id capture id
+     * @param {number|{value:string,currency_code:string}} amount refund amount
+     * @returns {Promise<*>}
+     */
+    async refundCapture ( id, amount ) {
+
+        const { currencyCode } = this.#config
+
+        const params = {
+            amount: 'number' === typeof amount ? {
+                value: amount.toFixed ( 2 ),
+                currency_code: currencyCode
+            } : amount
+        }
+
+        const { body } = await IO.http ( {
+            url: this.urls.refundCapture.replace ( '${id}', id ),
+            headers: await this.genHeaders ( )
+        }, params )
+
+        return body
+    }
+
+
+
+    /**
+     * Show refund details
+     * @param id refund id
+     * @returns {Promise<*>}
+     */
+    async getRefunds ( id ) {
+
+        const { body } = await IO.http ( {
+            url: this.urls.getRefunds.replace ( '${id}', id ),
+            headers: await this.genHeaders ( )
+        } )
 
         return body
     }
